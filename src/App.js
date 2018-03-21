b1ee4e3a7da1417682814b7b4b310a01b1e5d214
WGC575 <wgc575@gmail.com>
2018-03-21 13:13:22 -0700

    bubble chart and pie chart

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import rd3 from 'rd3';
 import d3 from 'd3';
 import ReactDOM from 'react-dom';
+import ReactBubbleChart from 'react-bubble-chart';
 
-var LineChart = rd3.LineChart
+var LineChart = rd3.LineChart;
 var dataSrc = [
   {
     "nodes": [
@@ -53,11 +54,32 @@ var dataSrc = [
   }
 ];
 
+var colorLegend = [
+  //reds from dark to light
+  /*{color: "#67000d", text: 'Negative', textColor: */"#ffffff"/*}*/, "#a50f15", "#cb181d", "#ef3b2c", "#fb6a4a", "#fc9272", "#fcbba1", "#fee0d2",
+  //neutral grey
+  /*{color: */"#f0f0f0"/*, text: 'Neutral'}*/,
+  // blues from light to dark
+  "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", /*{color: "#08306b", text: 'Positive', textColor: */"#ffffff"/*}*/
+];
+
+var tooltipProps = [{
+  css: 'symbol',
+  prop: '_id'
+}, {
+  css: 'value',
+  prop: 'value',
+  display: 'Commits'
+}/*, {
+  css: 'change',
+  prop: 'colorValue',
+  display: 'Change'
+}*/];
 
 var data = {
   'series 1': [ 
-    { x: 1423915030039, y: 11.2 },
-    { x: 1423913330040, y: 2.1 }
+    { x: '2014-07-10', y: 1.2 },
+    { x: '2014-10-15', y: 22.1 }
   ]
 };
 
@@ -70,93 +92,152 @@ for (var i = 0; i < dataSrc[0].nodes.length; i++) {
 
 var lineData_Dev = [
       { 
-        name: 'Number of developers',
+        name: 'Project_1',
         values: [   
-            { x: 0, y: 20 }, 
-            { x: dataSrc[0].nodes[1].time, y: 30 }, 
-            { x: dataSrc[0].nodes[2].time, y: 10 }, 
-            { x: dataSrc[0].nodes[3].time, y: 5 }, 
-            { x: dataSrc[0].nodes[4].time, y: 8 }, 
-            { x: dataSrc[0].nodes[5].time, y: 15 }, 
-            { x: dataSrc[0].nodes[6].time, y: 10 } ]
+            { x: '2016-11-22 14:41:58.0', y: 2 }, 
+            { x: '2016-11-29 14:41:58.0', y: 4 },
+            { x: '2016-12-6 14:41:58.0', y: 5 }, 
+            { x: '2016-12-13 14:41:58.0', y: 6 }
+        ]
       }
 ];
     
 var lineData_Edit = [
       { 
-        name: 'Edit times',
+        name: 'Project_1',
         values: [   
-            { x: 0, y: 20 }, 
-            { x: 1, y: 30 }, 
-            { x: 2, y: 10 }, 
-            { x: 3, y: 5 }, 
-            { x: 4, y: 8 }, 
-            { x: 5, y: 15 }, 
-            { x: 6, y: 10 } ]
+            { x: '2016-11-22 14:41:58.0', y: 5 }, 
+            { x: '2016-11-29 14:41:58.0', y: 20 },
+            { x: '2016-12-6 14:41:58.0', y: 40 }, 
+            { x: '2016-12-13 14:41:58.0', y: 62 }
+        ]
       }
 ];
-//var formatter = d3.time.format("%Y-%m-%d %H:%M:%S %Y").parse;
+
+//var formatter = d3.time.format("%Y-%m-%d %H:%M:%S.%Y").parse;
+
+
+//pie chart
+var PieChart = rd3.PieChart
+var pieData = [
+  {label: "ggregory@apache.org", value: 70.0}, 
+  {label: "aabrasha@gmail.com", value: 10.0}, 
+  {label: "tmartin193@gmail.com", value: 10.0 }, 
+  {label: "pascalschumacher@gmx.net", value: 10.0 }
+];
 
 class App extends Component {
   render() {
+       var data = [
+      {
+        _id: "ggregory@apache.org",        // unique id (required)
+        value: 7,      // used to determine relative size of bubbles (required)
+        displayText: "7",// will use _id if undefined
+        colorValue: -4, // used to determine color
+        selected: false,  // if true will use selectedColor/selectedTextColor for circle/text
+      },
+      {
+        _id: "aabrasha@gmail.com",  
+        value: 1,      
+        displayText: "1",
+        colorValue: -2, 
+        selected: false,
+      },
+      {
+        _id: "tmartin193@gmail.com",   
+        value: 1,      
+        displayText: "1",
+        colorValue: 1, 
+        selected: false,  
+      },
+      {
+        _id: "pascalschumacher@gmx.net",      
+        value: 1,     
+        displayText: "1",
+        colorValue: 3, 
+        selected: false,  
+      }
+    ]
     return (
       <div id = "123">
-          {(dataSrc[0].nodes[2].time)}
           <div>
           <LineChart
             legend={true}
             data={lineData_Dev}
-            width='40%'
+            width='50%'
             height={400}
             viewBoxObject={{
               x: 0,
               y: 0,
-              width: 500,
+              width: 1000,
               height: 300
             }}
             title="Number of developers along the time"
-            yAxisLabel="Altitude"
-            xAxisLabel="Elapsed Time (sec)"
+            yAxisLabel="Number of developers"
+            xAxisLabel="Date"
             gridHorizontal={true}
-            /*xAccessor={
-                (d)=> {
-                    return formatter(d.x);
-                }     
-            }*/
+            xAccessor={(d)=> {
+              var formatter = d3.time.format("%Y-%m-%d %H:%M:%S.%Y").parse;
+              return formatter(d.x);
+            }}
+            domain={{x: [,], y: [0,]}}
           />
           </div>
           <div>
           <LineChart
             legend={true}
             data={lineData_Edit}
-            width='40%'
+            width='50%'
             height={400}
             viewBoxObject={{
               x: 0,
               y: 0,
-              width: 500,
+              width: 1000,
               height: 300
             }}
             title="Edit times along the time"
-            yAxisLabel="Altitude"
-            xAxisLabel="Elapsed Time (sec)"
+            yAxisLabel="Number of Edits"
+            xAxisLabel="Date"
             gridHorizontal={true}
+            xAccessor={(d)=> {
+              var formatter = d3.time.format("%Y-%m-%d %H:%M:%S.%Y").parse;
+              return formatter(d.x);
+            }}
+            domain={{x: [,], y: [0,]}}
           />
           </div>
           <div>
-              <LineChart
+            <ReactBubbleChart
+              className="my-cool-chart"
+              colorLegend={colorLegend}
               data={data}
-              xAccessor={(d)=> {
-                  return new Date(d.x);
-                }     
-              }
-              yAccessor={(d)=>d.y}
-              xAxisTickInterval={{unit: 'year', interval: 2}}
-            />    
+              //selectedColor="#737373"
+              //selectedTextColor="#d9d9d9"
+              fixedDomain={{min: -5, max: 5}}
+              tooltip={true}
+              tooltipProps={tooltipProps}
+              //onClick={Actions.doStuff.bind(Actions)}
+              legend={true}
+              legendSpacing={1}
+            />
           </div>
+          <div>
+            <PieChart
+              data={pieData}
+              width={650}
+              height={400} 
+              radius={110}
+              innerRadius={40}
+              sectorBorderColor="white"
+              title="Contribution of authors" 
+            />
+          </div>
+          
+
       </div>
     );
   }
 }
+
 
 export default App;
